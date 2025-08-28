1c460e9d1a29bf123556263671f6cbe0a7082f41
prayaanah-travalen <aws@prayaanah.com>
2025-08-28 23:18:10 +0530

    Update V1.6__create_common_param.sql

--- admin-backend/travelo-admin/src/main/resources/db/migration/V1.6__create_common_param.sql
+++ admin-backend/travelo-admin/src/main/resources/db/migration/V1.6__create_common_param.sql
@@ -1,4 +1,4 @@
-create table travalen.common_parameter(id BIGSERIAL NOT NULL,
+create table common_parameter(id BIGSERIAL NOT NULL,
 parameter_id varchar(50),
 parameter_group varchar(50),
 parameter varchar(50),
@@ -7,7 +7,7 @@ available boolean
 );
 
 
-insert into travalen.common_parameter(parameter_id,parameter_group,parameter,description, available) values
+insert into common_parameter(parameter_id,parameter_group,parameter,description, available) values
 ('PCK0001','PACKAGE','accommodation-only','Accommodation Only', true),
 ('PCK0002','PACKAGE','breakfast-only','Breakfast Only', true),
 ('PCK0003','PACKAGE','breakfast-with-dinner','Breakfast With Dinner', true),
