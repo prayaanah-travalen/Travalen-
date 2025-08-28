a53c721073e91c48bd5b8c1ef9dfacc7334efa8c
prayaanah-travalen <aws@prayaanah.com>
2025-08-28 23:14:06 +0530

    Update V1.10__alter_table.sql

--- admin-backend/travelo-admin/src/main/resources/db/migration/V1.10__alter_table.sql
+++ admin-backend/travelo-admin/src/main/resources/db/migration/V1.10__alter_table.sql
@@ -1,7 +1,7 @@
-ALTER table travalen.finance
+ALTER table finance
 ADD column if not exists  pan varchar(50);
 
-ALTER table travalen.hotel_room
+ALTER table hotel_room
 ADD column if not exists extra_bed_cost varchar(50),
 ADD column if not exists extra_child_bed_cost varchar(50);
 
