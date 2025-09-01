fa3ebedd6e9508d832a425405a6c6ac28b926dce
hasnahussain <hasnahussain217@gmail.com>
2025-09-01 11:30:05 +0530

    Updated WebSecurityConfig.java with security config changes

--- New_admin-backend/travelo-admin/src/main/java/com/app/travelo/config/WebSecurityConfig.java
+++ New_admin-backend/travelo-admin/src/main/java/com/app/travelo/config/WebSecurityConfig.java
@@ -88,8 +88,11 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200","https://admin.travalen.in","https://www.admin.travalen.in","http://admin-travelo.ap-south-1.elasticbeanstalk.com","http://admin-travalen.s3-website.ap-south-1.amazonaws.com")); //or add * to allow all origins
         configuration.setAllowCredentials(true);
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")); //to set allowed http methods
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
-        configuration.setExposedHeaders(Arrays.asList("custom-header1", "custom-header2"));
+        // configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        // configuration.setExposedHeaders(Arrays.asList("custom-header1", "custom-header2"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setExposedHeaders(Arrays.asList("*"));
+        
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
