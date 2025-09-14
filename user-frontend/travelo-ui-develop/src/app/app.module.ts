fbfc136459a90dabd7ddbcd8dee41486775bdbfa
hasnahussain <hasnahussain217@gmail.com>
2025-09-14 15:46:28 +0530

    Fixed syntax issue in app.module.ts by closing AgmCoreModule.forRoot properly

--- user-frontend/travelo-ui-develop/src/app/app.module.ts
+++ user-frontend/travelo-ui-develop/src/app/app.module.ts
@@ -42,7 +42,7 @@ import { AlertComponent } from './shared/alert/alert.component';
       // please get your own API key here:
       // https://developers.google.com/maps/documentation/javascript/get-api-key?hl=en
      apiKey: 'AIzaSyAvcDy5ZYc2ujCS6TTtI3RYX5QmuoV8Ffw',
-    //}),
+    }),
   ],
   providers: [ LoaderService,
     {
