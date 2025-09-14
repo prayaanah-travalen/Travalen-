4f966d171be7953b5d8bc1a5df6b60ea6f632496
hasnahussain <hasnahussain217@gmail.com>
2025-09-14 16:08:14 +0530

    Refactor AppModule: clean imports, organize modules, configure AgmCoreModule

--- user-frontend/travelo-ui-develop/src/app/app.module.ts
+++ user-frontend/travelo-ui-develop/src/app/app.module.ts
@@ -1,50 +1,62 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MatDialogModule } from '@angular/material/dialog';
-import { SearchPageModule } from './search-page/search-page.module';
-import { HotelDetailsModule } from './hotel-details/hotel-details.module';
-import { DemoComponent } from './demo/demo.component';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { LoaderInterceptorService } from './shared/app-loader/services/loader-interceptor.service';
-import { LoaderService } from './shared/app-loader/services/loader.service';
-import { AppLoaderModule } from './shared/app-loader/app-loader.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
+// Routing
+import { AppRoutingModule } from './app-routing.module';
+
+// Material & UI
+import { MatDialogModule } from '@angular/material/dialog';
 import { ToastrModule } from 'ngx-toastr';
-import { AgmCoreModule } from '@agm/core';
-import { GoogleMapComponent } from './shared/google-map/google-map.component';
+
+// App Components
+import { AppComponent } from './app.component';
+import { DemoComponent } from './demo/demo.component';
+
+// Shared Modules
+import { AppLoaderModule } from './shared/app-loader/app-loader.module';
+// import { GoogleMapComponent } from './shared/google-map/google-map.component'; // optional: declare in SharedModule if needed
 import { AlertComponent } from './shared/alert/alert.component';
 
-@NgModule({
+// Feature Modules
+import { SearchPageModule } from './search-page/search-page.module';
+import { HotelDetailsModule } from './hotel-details/hotel-details.module';
 
+// Services
+import { LoaderService } from './shared/app-loader/services/loader.service';
+import { LoaderInterceptorService } from './shared/app-loader/services/loader-interceptor.service';
+
+// Google Maps
+import { AgmCoreModule } from '@agm/core'; // or migrate to @angular/google-maps
+
+@NgModule({
   declarations: [
     AppComponent,
     DemoComponent,
-
-    // GoogleMapComponent,
-    
-  
-    
+    // AlertComponent, // if used in AppComponent template
+    // GoogleMapComponent, // should ideally be in a SharedModule
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     MatDialogModule,
-    SearchPageModule,
-    HotelDetailsModule,
     HttpClientModule,
     AppLoaderModule,
-    BrowserAnimationsModule,
     ToastrModule.forRoot(),
+
+    // Feature Modules
+    SearchPageModule,
+    HotelDetailsModule,
+
+    // Google Maps Module
     AgmCoreModule.forRoot({
-      // please get your own API key here:
-      // https://developers.google.com/maps/documentation/javascript/get-api-key?hl=en
-     apiKey: 'AIzaSyAvcDy5ZYc2ujCS6TTtI3RYX5QmuoV8Ffw',
+      apiKey:'AIzaSyAvcDy5ZYc2ujCS6TTtI3RYX5QmuoV8Ffw'
     }),
   ],
-  providers: [ LoaderService,
+  providers: [
+    LoaderService,
     {
       provide: HTTP_INTERCEPTORS,
       useClass: LoaderInterceptorService,
@@ -52,6 +64,5 @@ import { AlertComponent } from './shared/alert/alert.component';
     },
   ],
   bootstrap: [AppComponent],
-  exports:[]
 })
 export class AppModule { }
